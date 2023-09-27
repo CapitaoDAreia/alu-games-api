a61cfdbdbb56e3308cf1cd1a83d1bc2b83de6c8c
CapitaoDAreia <danieligord7@hotmail.com>
2023-09-27 15:42:31 -0300

    reading user input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,19 @@ import com.google.gson.Gson
 import domain.entities.Game
 import domain.entities.InfoGame
 import providers.getGame
+import java.util.Scanner
 import kotlin.Exception
 
 fun main() {
 
-    val response = getGame("https://www.cheapshark.com/api/1.0/games?id=146")
+    val scanner = Scanner(System.`in`)
+    println("Type an game ID")
+    val id = scanner.nextLine()
 
+    val response = getGame("https://www.cheapshark.com/api/1.0/games?id=$id")
     val gson = Gson()
-
     val infoGame = gson.fromJson(response.body(), InfoGame::class.java) ?: throw Exception("info game is null")
 
     val game = Game(infoGame.info.title, infoGame.info.thumb)
-
     println(game)
-
 }
